f9d12514a44b0daaab55f9b86e43659a90620184
Alexrerx <rerx@yandex.ru>
2016-01-07 11:56:18 +0300

    Initial commit

---
+++ app/src/main/java/com/rerx/alexey/bmstu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rerx.alexey.bmstu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
