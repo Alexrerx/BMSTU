b6d07be78e1911f85cc17cfd0716568b10da63d0
Alexrerx <rerx@yandex.ru>
2016-01-07 12:11:23 +0300

    Record

--- app/src/main/java/com/rerx/alexey/bmstu/MainActivity.java
+++ app/src/main/java/com/rerx/alexey/bmstu/MainActivity.java
@@ -1,13 +1,125 @@
 package com.rerx.alexey.bmstu;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*Константы*/
+    TextView txt;
+    public static final String FILE_FOLDER = "records";
+    public static final String PATH = android.os.Environment
+            .getExternalStorageDirectory()
+            + java.io.File.separator
+            + FILE_FOLDER + java.io.File.separator;
+    /*------------------------------*/
+    private String fileName;
+    /*****************DEBUG*******************/
+    String fileDebug;
+    public static final String FILE_DEBUG_FOLDER = "debug";
+    public static final String PATHDEBUG = android.os.Environment
+            .getExternalStorageDirectory()
+            + java.io.File.separator
+            + FILE_DEBUG_FOLDER + java.io.File.separator;
+
+/**************************************/
+
+    /**************ЧТЕНИЕ***************/
+    Record record = new Record(this);
+    int sampleRate = 8000;
+    int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+    int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+
+    int minInternalBufferSize = AudioRecord.getMinBufferSize(sampleRate,
+            channelConfig, audioFormat);
+    int internalBufferSize = minInternalBufferSize * 4;
+
+    AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
+            sampleRate, channelConfig, audioFormat, internalBufferSize);
+
+    short [] audioData = new short[internalBufferSize];//предположительно массив данных
+
+    /***************************************/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        txt = new TextView(this);
+        Button buttonDebug = new Button(this);
+        buttonDebug = (Button) findViewById(R.id.buttonDebug);
+        buttonDebug.setOnClickListener(record.new OnClickListener());
+        Button buttonDebugStop = new Button(this);
+        buttonDebugStop = (Button) findViewById(R.id.stopDebug);
+        buttonDebugStop.setOnClickListener(record.new OnClickListener());
+        /* Создание директории записи */
+        File dir = new File(PATH);
+        dir.mkdirs();
+        fileName = Environment.getExternalStorageDirectory() + "/record.3gpp";
+        /*****************************/
+        /*************DEBUG****************/
+        File dirDebug = new File(PATHDEBUG);
+        dirDebug.mkdirs();
+        File f = new File(fileDebug);
+
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /**********************************/
+
+        txt = (TextView) findViewById(R.id.debug);
+        audioRecord.read(audioData,0,100);
+        for (int i=0; i < audioData.length;i++){
+            txt.setText(i+1 + " " +  audioData[i] + "\n");
+        }
+    }
+
+
+    // sampleRate = 44100
+    //оно будет вычислять частоту
+//    public static int calculate(int sampleRate, short [] audioData){
+//
+//        int numSamples = audioData.length;
+//        int numCrossing = 0;
+//        for (int p = 0; p < numSamples-1; p++)
+//        {
+//            if ((audioData[p] > 0 && audioData[p + 1] <= 0) ||
+//                    (audioData[p] < 0 && audioData[p + 1] >= 0))
+//            {
+//                numCrossing++;
+//            }
+//        }
+//        float numSecondsRecorded = (float)numSamples/(float)sampleRate;
+//        float numCycles = numCrossing/2;
+//        float frequency = numCycles/numSecondsRecorded;
+//
+//        return (int)frequency;
+//    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+//        Record record = new Record();
+//        record.releasePlayer();
+//        record.releaseRecorder();
     }
 }
---
+++ app/src/main/java/com/rerx/alexey/bmstu/Record.java
@@ -0,0 +1,193 @@
+package com.rerx.alexey.bmstu;
+
+/**
+ * Created by alexey on 07.01.16.
+ */
+import android.content.Context;
+import android.media.AudioRecord;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+/**
+ * Created by alexey on 05.01.16.
+ */
+public class Record {
+    PrintWriter writer;
+    //всё для записи
+    private MediaRecorder mediaRecorder;
+    private MediaPlayer mediaPlayer;
+    private AudioRecord audioRecord;
+    boolean isReading = false;
+    int myBufferSize = 8192;
+    byte[] myBuffer = new byte[myBufferSize];
+    MainActivity main;
+
+    Record(Context context){
+        main = (MainActivity) context;
+    }
+
+    public void recordStart(View v) {
+        audioRecord.startRecording();
+        int recordingState = audioRecord.getRecordingState();
+    }
+
+    public void recordStop(View v) {
+        audioRecord.stop();
+    }
+
+    public void readStart() {
+        try {
+            writer =  new PrintWriter(main.fileDebug);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        isReading = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (audioRecord == null)
+                    return;
+
+
+                int readCount = 0;
+                int totalCount = 0;
+                while (isReading) {
+                    readCount = audioRecord.read(myBuffer, 0, myBufferSize);
+                    totalCount += readCount;
+
+                    main.runOnUiThread(new Runnable(){
+                        @Override
+                        public void  run() {
+                            for (int i = 0; i < myBuffer.length; i++) {
+                                main.txt.setText(i + 1 + " " + myBuffer[i] + "\n");
+
+                                writer.print(i + 1 + " " + myBuffer[i] + "\n");
+
+                            }
+                        }
+                    });
+
+                } writer.close();
+            }
+        }).start();
+
+    }
+
+    public void readStop() {
+        isReading = false;
+    }
+
+    public class OnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.buttonDebug:
+                    readStart();
+                    break;
+                case R.id.stopDebug:
+                    readStop();
+                    break;
+            }
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    private String fileName;
+//    public void recordStart(View v) {
+//        try {
+//            releaseRecorder();
+//
+//            File outFile = new File(fileName);
+//            if (outFile.exists()) {
+//                outFile.delete();
+//            }
+//
+//            mediaRecorder = new MediaRecorder();
+//            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+//            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+//            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+//            mediaRecorder.setOutputFile(fileName);
+//            mediaRecorder.prepare();
+//            mediaRecorder.start();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//    public void recordStop(View v) {
+//        if (mediaRecorder != null) {
+//            mediaRecorder.stop();
+//        }
+//    }
+//    public void playStart(View v) {
+//        try {
+//            releasePlayer();
+//            mediaPlayer = new MediaPlayer();
+//            mediaPlayer.setDataSource(fileName);
+//            mediaPlayer.prepare();
+//            mediaPlayer.start();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public void playStop(View v) {
+//        if (mediaPlayer != null) {
+//            mediaPlayer.stop();
+//        }
+//    }
+//
+//    public void releaseRecorder() {
+//        if (mediaRecorder != null) {
+//            mediaRecorder.release();
+//            mediaRecorder = null;
+//        }
+//    }
+//
+//    public void releasePlayer() {
+//        if (mediaPlayer != null) {
+//            mediaPlayer.release();
+//            mediaPlayer = null;
+//        }
+//    }
+
+
